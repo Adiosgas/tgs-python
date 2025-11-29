0cd36e7eecba713fd3008c388ac95097b1ba7244
Adiosgas <rafif_effendi_33rpl@student.smktelkom-mlg.sch.id>
2025-11-30 00:10:50 +0700

    first commit

---
+++ latian.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+# Load dataset
+data = pd.read_csv('nilai_siswa.csv')
+# Display first few rows of the dataset
+print(data.head())
+print(data.info())
+print(data.describe()) 
+
+print("Rata-rata:", data['Nilai'].mean())
+print("Median:", data['Nilai'].median())
+print("Modus:", data['Nilai'].mode()[0])
+
+matematika = data[data['Mapel'] == 'Matematika']
+print(matematika)
+
+data.groupby('Mapel')['Nilai'].agg(['max','min'])
+
+rata = data.groupby('Mapel')['Nilai'].mean()
+rata.plot(kind='bar')
+plt.title('Rata-Rata Nilai per Mapel')
+plt.xlabel('Mata Pelajaran')
+plt.ylabel('Nilai Rata-Rata')
+plt.show()
+
+sns.boxplot(x='Mapel', y='Nilai', data=data)
+plt.title('Distribusi Nilai per Mapel')
+plt.show()
